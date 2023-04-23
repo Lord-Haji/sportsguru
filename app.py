1ff79c3165cce1977381712f44a7d13dfe05c581
Lord-Haji <lordhajisinbox@gmail.com>
2023-04-24 00:39:06 +0530

    Display some more stats and change layout

--- app.py
+++ app.py
@@ -68,17 +68,30 @@ else:
     if st.button('Predict Probability'):
         runs_left = target - score
         balls_left = 120 - (overs*6)
-        wickets = 10 - wickets
         crr = score/overs
         rrr = (runs_left*6)/balls_left
+        predicted_score = score + int(crr*balls_left/6)
+        
+        wickets = 10 - wickets
 
         input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
 
         result = pipe.predict_proba(input_df)
         loss = result[0][0]
         win = result[0][1]
-        st.header(batting_team + "- " + str(round(win*100, 2)) + "%")
-        st.header(bowling_team + "- " + str(round(loss*100, 2)) + "%")
+        st.header("Win Probability:")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write(batting_team + ": {:.2f}%".format(win*100))
+        with col2:
+            st.write(bowling_team + ": {:.2f}%".format(loss*100))
+        
+        st.header("Score Predictions:")
+        st.write("CRR (Current Run Rate): {:.2f}".format(crr))
+        st.write("RRR (Required Run Rate): {:.2f}".format(rrr))
+        st.write("Predicted Score: {}".format(predicted_score), "/", str(10 - wickets))
+        
+
         
         
 # Footer implementation
